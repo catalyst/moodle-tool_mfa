1eb70822ffe8ccb580daeacaef1618841e734810
Peter Burnett <peterburnett@catalyst-au.net>
2019-11-19 14:27:36 +1000

    Added unit test for get_next_user_factor

---
+++ tests/plugininfo_factor_test.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     tool_mfa
+ * @author      Peter Burnett <peterburnett@catalyst-au.net>
+ * @copyright   Catalyst IT
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class tool_mfa_plugininfo_factor_testcase extends advanced_testcase {
+    public function test_get_next_user_factor() {
+        $this->resetAfterTest(true);
+        // Create and login a user.
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+        // Test that with no enabled factors, false is returned.
+        $this->assertFalse(\tool_mfa\plugininfo\factor::get_next_user_factor());
+
+        // Setup enabled totp factor for user.
+        set_config('enabled', 1, 'factor_totp');
+        $totpfactor = \tool_mfa\plugininfo\factor::get_factor('totp');
+        $totpdata = [
+            'secret' => 'fakekey',
+            'devicename' => 'fakedevice'
+        ];
+        $this->assertTrue($totpfactor->setup_user_factor((object) $totpdata));
+
+        // Test that factor now appears (from STATE_UNKNOWN).
+        $this->assertEquals(\tool_mfa\plugininfo\factor::get_next_user_factor()->name, 'totp');
+
+        // Now pass this factor
+        $totpfactor->set_state(\tool_mfa\plugininfo\factor::STATE_PASS);
+        $this->assertFalse(\tool_mfa\plugininfo\factor::get_next_user_factor());
+
+        // Add in a no-input factor
+        set_config('enabled', 1, 'factor_auth');
+        $this->assertEquals(2, count(\tool_mfa\plugininfo\factor::get_enabled_factors()));
+
+        $authfactor = \tool_mfa\plugininfo\factor::get_factor('auth');
+        $this->assertTrue($authfactor->is_enabled());
+        $this->assertFalse($authfactor->has_setup());
+
+        // TODO: Enable when a no_input factor is ready
+        /*$this->assertEquals(2, count(\tool_mfa\plugininfo\factor::get_active_user_factor_types()));
+        $this->assertFalse(\tool_mfa\plugininfo\factor::get_next_user_factor());*/
+    }
+}
+
