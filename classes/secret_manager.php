1356c73070edb688bf6fe10be6f190417335606a
Peter Burnett <peterburnett@catalyst-au.net>
2020-09-03 11:24:19 +1000

    Added most core functions

--- classes/secret_manager.php
+++ classes/secret_manager.php
@@ -29,10 +29,16 @@ defined('MOODLE_INTERNAL') || die();
 
 class secret_manager {
 
+    const REVOKED = 'revoked';
+    const VALID = 'valid';
+    const NONVALID = 'nonvalid';
+
     private $factor;
+    private $sessionvar;
 
     public function __construct(string $factor) {
         $this->factor = $factor;
+        $this->sessionvar = "tool_mfa_secrets_{$this->factor}";
     }
 
     public function create_secret(int $expires, bool $session, string $secret = null){
@@ -53,12 +59,95 @@ class secret_manager {
             $this->add_secret_to_db($secret, $expires);
         }
     }
-    private function add_secret_to_db(string $secret, $expires){}
-    private function add_secret_to_session(){}
 
-    public function validate_secret(string $secret){}
-    private function check_secret_against_db(){}
-    private function check_secret_against_session(){}
+    private function add_secret_to_db(string $secret, int $expires) {
+        global $DB, $USER;
+        $expirytime = time() + $expires;
+
+        $data = [
+            'userid' => $USER,
+            'factor' => $this->factor,
+            'secret' => $secret,
+            'timecreated' => time(),
+            'expiry' => $expirytime,
+            'revoked' => 0
+        ];
+        $DB->insert_record('tool_mfa_secrets', $data);
+    }
+
+    private function add_secret_to_session(string $secret, int $expires) {
+        global $SESSION;
+        $expirytime = time() + $expires;
+
+        $data = [
+            'secret' => $secret,
+            'timecreated' => time(),
+            'expiry' => $expirytime,
+            'revoked' => 0
+        ];
+        $datastr = json_encode($data);
+
+        $SESSION->$this->sessionvar = $datastr;
+    }
+
+    public function validate_secret(string $secret) {
+        global $DB, $SESSION, $USER;
+
+        // Check Session first, more likely and less overhead.
+        $status = $this->check_secret_against_session($secret);
+        if ($status !== self::NONVALID) {
+            if ($status === self::VALID) {
+                // Remove session token.
+                unset($SESSION->$this->sessionvar);
+            }
+            return $status;
+        }
+
+        // Now DB.
+        $status = $this->check_secret_against_db($secret);
+        if ($status !== self::NONVALID) {
+            if ($status === self::VALID) {
+                // Cleanup DB $record.
+                $DB->delete_records('tool_mfa_secrets', ['userid' => $USER->id, 'factor' => $this->factor]);
+            }
+            return $status;
+        }
+
+        //This is always nonvalid.
+        return $status;
+    }
+
+    private function check_secret_against_db(string $secret) {
+        global $DB;
+
+        $sql = 'SELECT *
+                  FROM {tool_mfa_secrets}
+                 WHERE secret = :secret
+                   AND expiry > :now';
+
+        $record = $DB->get_record_sql($sql, ['secret' => $secret, 'now' => time()]);
+
+        if (!empty($record)) {
+            if ($record->revoked) {
+                return self::REVOKED;
+            }
+            return self::VALID;
+        }
+        return self::NONVALID;
+    }
+
+    private function check_secret_against_session(string $secret) {
+        global $SESSION;
+
+       if (!empty($SESSION->this->sessionvar)) {
+           $data = json_decode($SESSION->$this->sessionvar);
+           if ($data->revoked) {
+               return self::REVOKED;
+           }
+           return self::VALID;
+       }
+       return self::NONVALID;
+    }
 
     public function revoke_secret(string $secret){}
 
