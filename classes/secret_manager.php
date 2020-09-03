12355d7e243aa76f5c986ef6d031d2ecb430ead5
Peter Burnett <peterburnett@catalyst-au.net>
2020-09-03 14:27:11 +1000

    API Documentation

--- classes/secret_manager.php
+++ classes/secret_manager.php
@@ -41,14 +41,22 @@ class secret_manager {
         $this->sessionvar = "tool_mfa_secrets_{$this->factor}";
     }
 
-    public function create_secret(int $expires, bool $session, string $secret = null){
+    /**
+     * This function creates or takes a secret, and stores it in the database or session.
+     *
+     * @param integer $expires the length of time the secret is valid. e.g. 1 min = 60
+     * @param boolean $session whether this secret should be stored in the session.
+     * @param string $secret an optional provided secret
+     * @return void
+     */
+    public function create_secret(int $expires, bool $session, string $secret = null) : void {
         // Setup a secret if not provided.
         if (empty($secret)) {
             $secret = random_int(100000, 999999);
         }
 
-        // Check if there already an active secret.
-        if ($this->has_active_secret()) {
+        // Check if there already an active secret, unless we are forcibly given a code.
+        if ($this->has_active_secret() && $secret) {
             return;
         }
 
@@ -60,7 +68,14 @@ class secret_manager {
         }
     }
 
-    private function add_secret_to_db(string $secret, int $expires) {
+    /**
+     * Inserts the provided secret into the database with a given expiry duration.
+     *
+     * @param string $secret the secret to store
+     * @param integer $expires expiry duration in seconds
+     * @return void
+     */
+    private function add_secret_to_db(string $secret, int $expires) : void {
         global $DB, $USER;
         $expirytime = time() + $expires;
 
@@ -75,7 +90,14 @@ class secret_manager {
         $DB->insert_record('tool_mfa_secrets', $data);
     }
 
-    private function add_secret_to_session(string $secret, int $expires) {
+    /**
+     * Inserts the provided secret into the session with a given expiry duration.
+     *
+     * @param string $secret the secret to store
+     * @param integer $expires expiry duration in seconds
+     * @return void
+     */
+    private function add_secret_to_session(string $secret, int $expires) : void {
         global $SESSION;
         $expirytime = time() + $expires;
 
@@ -90,7 +112,13 @@ class secret_manager {
         $SESSION->$field = $datastr;
     }
 
-    public function validate_secret(string $secret) {
+    /**
+     * Validates whether the provided secret is currently valid.
+     *
+     * @param string $secret the secret to check
+     * @return string a secret manager state constant
+     */
+    public function validate_secret(string $secret) : string {
         global $DB, $SESSION, $USER;
 
         // Check Session first, more likely and less overhead.
@@ -118,7 +146,13 @@ class secret_manager {
         return $status;
     }
 
-    private function check_secret_against_db(string $secret) {
+    /**
+     * Checks if a given secret is valid from the Database.
+     *
+     * @param string $secret the secret to check.
+     * @return string a secret manager state constant.
+     */
+    private function check_secret_against_db(string $secret) : string {
         global $DB, $USER;
 
         $sql = "SELECT *
@@ -139,13 +173,21 @@ class secret_manager {
         return self::NONVALID;
     }
 
-    private function check_secret_against_session(string $secret) {
+    /**
+     * Checks whether a given secret is valid in the session.
+     *
+     * @param string $secret the secret to check
+     * @return string a secret manager state constant
+     */
+    private function check_secret_against_session(string $secret) : string {
         global $SESSION;
 
         $field = $this->sessionvar;
         if (!empty($SESSION->$field)) {
            $data = json_decode($SESSION->$field);
-           if ($data->revoked) {
+           if ($data->secret !== $secret || $data->expiry < time()) {
+               return self::NONVALID;
+           } else if ($data->revoked) {
                return self::REVOKED;
            }
            return self::VALID;
@@ -153,12 +195,18 @@ class secret_manager {
        return self::NONVALID;
     }
 
-    public function revoke_secret(string $secret) {
+    /**
+     * Revokes the provided secret code for the user.
+     *
+     * @param string $secret the secret to revoke.
+     * @return void
+     */
+    public function revoke_secret(string $secret) : void {
         // Check session first. Faster and more likely.
         $status = $this->check_secret_against_session($secret);
         if ($status === self::VALID) {
             // We only need to do something if this is a valid secret.
-            $this->revoke_session_secret($secret);
+            $this->revoke_session_secret();
         }
 
         // Now DB.
@@ -168,7 +216,12 @@ class secret_manager {
         }
     }
 
-    private function revoke_session_secret(string $secret) : void {
+    /**
+     * Revokes the current session level secret.
+     *
+     * @return void
+     */
+    private function revoke_session_secret() : void {
         global $SESSION;
 
         $field = $this->sessionvar;
@@ -179,14 +232,25 @@ class secret_manager {
         $SESSION->$field = json_encode($data);
     }
 
+    /**
+     * Revokes a DB secret.
+     *
+     * @param string $secret
+     * @return void
+     */
     private function revoke_db_secret(string $secret) : void {
         global $DB, $USER;
         // We know this secret is valid, so we don't need to check expiry.
         $DB->set_field('tool_mfa_secrets', 'revoked', 1, ['userid' => $USER->id, 'factor' => $this->factor, 'secret' => $secret]);
     }
 
+    /**
+     * Checks whether this factor currently has an active secret, and should not add another.
+     *
+     * @return boolean
+     */
     private function has_active_secret() : bool{
-        global $DB, $SESSION;
+        global $DB, $SESSION, $USER;
 
         $field = $this->sessionvar;
         // Check for session first.
