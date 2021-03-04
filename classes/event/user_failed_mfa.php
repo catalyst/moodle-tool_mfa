5d3ca7ec214c6c7a2341cf6fd56e1083f17ab70f
Peter Burnett <peterburnett@catalyst-au.net>
2021-03-05 08:45:29 +1000

    Fixed regression in resetting non-input factors

--- classes/event/user_failed_mfa.php
+++ classes/event/user_failed_mfa.php
@@ -54,11 +54,13 @@ class user_failed_mfa extends \core\event\base {
         // Build debug info string.
         $factors = \tool_mfa\plugininfo\factor::get_active_user_factor_types();
         $debug = '';
-        $failurereason = get_string('event:faillockout', 'tool_mfa');
+        $failurereason = get_string('event:failnotenoughfactors', 'tool_mfa');
         foreach ($factors as $factor) {
             $debug .= "<br> Factor {$factor->name} status: {$factor->get_state()}";
             if ($factor->get_state() === \tool_mfa\plugininfo\factor::STATE_FAIL) {
                 $failurereason = get_string('event:failfactor', 'tool_mfa');
+            } else if ($factor->get_state() === \tool_mfa\plugininfo\factor::STATE_LOCKED) {
+                $failurereason = get_string('event:faillockout', 'tool_mfa');
             }
         }
 
